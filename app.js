0fcb0e01a2d7a26dff7001353606bffecbc54c07
Aditya Aryan <adityaaryan77@gmail.com>
2023-04-29 05:29:22 +0530

    Complete V2 with Database

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -23,9 +32,11 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
+  Post.find().then((foundPosts) => {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: foundPosts,
+    });
   });
 });
 
@@ -42,28 +53,28 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+  const newPost = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-
-  posts.push(post);
-
+  });
+  newPost.save();
   res.redirect("/");
 });
 
 app.get("/posts/:postName", function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
+  Post.find().then((foundPosts) => {
+    foundPosts.forEach((post) => {
+      const storedTitle = _.lowerCase(post.title);
 
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      });
-    }
+      if (storedTitle === requestedTitle) {
+        res.render("post", {
+          title: post.title,
+          content: post.content,
+        });
+      }
+    });
   });
 });
 
